0424f4b7f238c6f79abcdb7d1548523ad52fbae2
Kalliu Brasil <kalliu.brasil@zrp.com.br>
2024-02-09 20:10:04 -0300

    chore: Improving folder organization

--- app/routes/auth.keycloak.tsx
+++ app/routes/auth.keycloak.tsx
@@ -1,6 +1,6 @@
 import { redirect, type ActionFunction, type LoaderFunction } from "@remix-run/node";
 
-import { authenticator } from "~/utils/auth.server";
+import { authenticator } from "~/services/auth.server";
 
 export const loader: LoaderFunction = () => redirect("/");
 
